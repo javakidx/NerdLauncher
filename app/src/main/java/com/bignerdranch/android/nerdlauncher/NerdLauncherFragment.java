67fd6c33c4810151b71d21b90820001f8d4d77f6
javakidx <javakidx@gmail.com>
2015-08-09 01:07:43 +0800

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/nerdlauncher/NerdLauncherFragment.java
@@ -0,0 +1,10 @@
+package com.bignerdranch.android.nerdlauncher;
+
+import android.support.v4.app.ListFragment;
+
+/**
+ * Created by bioyang on 15/8/9.
+ */
+public class NerdLauncherFragment extends ListFragment
+{
+}
